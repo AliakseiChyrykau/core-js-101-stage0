073f45cc6d54c9e613fab748aae77d05bf00e902
Aliaksei Chyrykau <L_chr@mail.ru>
2025-08-14 23:06:41 +0300

    feat: task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -180,8 +180,12 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  let tempNum = num / 10 ** pow;
+
+  tempNum = Math.round(tempNum);
+
+  return tempNum * 10 ** pow;
 }
 
 /**
@@ -201,8 +205,18 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n <= 1) {
+      return false;
+    }
+
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -220,8 +234,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) || def;
 }
 
 module.exports = {
